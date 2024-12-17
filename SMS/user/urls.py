3e737932de4c754c0cc59fd69cb67fe01f4f3cd5
Jaimon12 <appupadinjarayil39@gmail.com>
2024-12-17 19:44:33 +0530

    Describe the changes

---
+++ SMS/user/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from user.views import RegisterAPIView,LoginAPIView,CreateOfficeStaffView,UserDetailView,LibraryReviewView,CreateLibrarianView,StudentView,LibraryHistoryAPIView,FeesHistoryAPIView
+
+urlpatterns = [
+    
+        path('register/', RegisterAPIView.as_view(), name='register'),
+        path('login/', LoginAPIView.as_view(), name='login'),
+        path('create-office-staff/', CreateOfficeStaffView.as_view(), name='create-office-staff'),
+        path('create-librarian/', CreateLibrarianView.as_view(), name='create-librarian'),
+        path('create-student/', StudentView.as_view(), name='create-student'),
+        path('library-history/', LibraryHistoryAPIView.as_view(), name='library-history'),
+        path('fees-history/', FeesHistoryAPIView.as_view(), name='fees-history'),
+        path('LibraryReview/', LibraryReviewView.as_view(), name='LibraryReviewView'),
+        path('user/', UserDetailView.as_view(), name='user-detail'),
+
+     
+]
